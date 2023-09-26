ba13593a95690679a557a094f78990e82ad11d97
Антон <loparuxin.a@gmail.com>
2023-09-26 21:02:31 +0300

    Стартовый проект

---
+++ src/main/java/ru/procodehub/ProCodeHubApplication.java
@@ -0,0 +1,13 @@
+package ru.procodehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProCodeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProCodeHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/procodehub/ProCodeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.procodehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProCodeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
